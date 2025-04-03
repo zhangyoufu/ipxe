5f89195523200d547841051e242deba8bf2ec0cf
Youfu Zhang <zhangyoufu@gmail.com>
2025-04-04 07:40:28 +0800

    enable CLI commands used by netboot.xyz scripts

--- src/config/general.h
+++ src/config/general.h
@@ -152,7 +152,7 @@ FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
 #define SHELL_CMD		/* Shell command */
 //#define NSLOOKUP_CMD		/* DNS resolving command */
 //#define TIME_CMD		/* Time commands */
-//#define DIGEST_CMD		/* Image crypto digest commands */
+#define DIGEST_CMD		/* Image crypto digest commands */
 //#define LOTEST_CMD		/* Loopback testing commands */
 //#define VLAN_CMD		/* VLAN commands */
 //#define PXE_CMD		/* PXE commands */
@@ -160,11 +160,11 @@ FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
 #define POWEROFF_CMD		/* Power off command */
 #define IMAGE_TRUST_CMD	/* Image trust management commands */
 //#define IMAGE_CRYPT_CMD	/* Image encryption management commands */
-//#define PCI_CMD		/* PCI commands */
+#define PCI_CMD		/* PCI commands */
 //#define PARAM_CMD		/* Request parameter commands */
 //#define NEIGHBOUR_CMD		/* Neighbour management commands */
 //#define PING_CMD		/* Ping command */
-//#define CONSOLE_CMD		/* Console command */
+#define CONSOLE_CMD		/* Console command */
 //#define IPSTAT_CMD		/* IP statistics commands */
 //#define PROFSTAT_CMD		/* Profiling commands */
 //#define NTP_CMD		/* NTP commands */
