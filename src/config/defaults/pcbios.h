4cbc5927010d8a4509c98bfa1f817786e9bf2a02
Youfu Zhang <zhangyoufu@gmail.com>
2025-04-04 08:12:51 +0800

    tailored for netboot.xyz
    minimize build time
    minimize feature set
    minimize image size

--- src/config/defaults/pcbios.h
+++ src/config/defaults/pcbios.h
@@ -19,7 +19,6 @@ FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
 #define UMALLOC_MEMTOP
 #define SMBIOS_PCBIOS
 #define SANBOOT_NULL
-#define ENTROPY_RTC
 #define ENTROPY_RDRAND
 #define TIME_RTC
 #define REBOOT_PCBIOS
@@ -32,8 +31,8 @@ FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
 #define IOMAP_VIRT
 #endif
 
-#define	IMAGE_ELF		/* ELF image support */
-#define	IMAGE_MULTIBOOT		/* MultiBoot image support */
+//#define	IMAGE_ELF		/* ELF image support */
+//#define	IMAGE_MULTIBOOT		/* MultiBoot image support */
 #define	IMAGE_PXE		/* PXE image support */
 #define IMAGE_SCRIPT		/* iPXE script image support */
 #define IMAGE_BZIMAGE		/* Linux bzImage image support */
